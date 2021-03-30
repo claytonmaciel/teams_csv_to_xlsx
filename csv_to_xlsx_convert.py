f0eddbe0af2d39f3c3d60d7917fc975dca655ed5
Clayton Maciel <62034289+claytonmaciel@users.noreply.github.com>
2021-03-30 10:43:08 -0300

    Update csv_to_xlsx_convert.py

--- csv_to_xlsx_convert.py
+++ csv_to_xlsx_convert.py
@@ -6,7 +6,7 @@ from pandas import ExcelWriter
 class App(QWidget):
     def __init__(self):
         super().__init__()
-        self.title = 'TeamsCSVToXLSX Converter'
+        self.title = 'TeamsCSVToXLSX Convert'
         self.initUI()
 
     def initUI(self):
